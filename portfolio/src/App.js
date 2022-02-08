91cbe18d0c161afa94860653f7bcf4f04b79da92
agustinfranchetti <agustinfranchetti@gmail.com>
2022-02-08 17:53:11 -0300

    added Typical

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Profile from './PortfolioContainer/Home/Profile'
 
 function App() {
--- portfolio/src/PortfolioContainer/Home/Profile.js
+++ portfolio/src/PortfolioContainer/Home/Profile.js
@@ -1,23 +1,53 @@
 import React from 'react';
+import Typical from 'react-typical';
 
 export default function Profile() {
   return <div className='profile-container'>
       <div className='profile-parent'>
           <div className='profile-details'>
               <div className='colz'>
-                  <a href='#'>
+                  <a href='https://www.instagram.com/agustinfranchetti'>
                       <i className='fa fa-instagram'></i>
                   </a>
-                  <a href='#'>
+                  <a href='https://www.twitter.com/agusfrancho'>
                       <i className='fa fa-twitter-square'></i>
                   </a>
-                  <a href='#'>
+                  <a href='https://www.linkedin.com/in/agust%C3%ADn-franchetti-a98a6a105/'>
                       <i className='fa fa-linkedin-square'></i>
                   </a>
-                  <a href='#'>
+                  <a href='https://www.github.com/agustinfranchetti'>
                       <i className='fa fa-github-square'></i>
                   </a>
               </div>
+              <div className='profile-details-name'>
+                <span className='primary-text'>
+                    {" "}
+                    Hi, I'm <span className='highlighted-text'>Agustín</span> 👋🏻
+                </span>
+              </div>
+              <div className='profile-details-role'>
+                  <span className='primary-text'>
+                      <h1>
+                        {" "}
+                        <Typical
+                        loop={Infinity}
+                        steps={[
+                            "Fullstack Developer",
+                            1000,
+                            "Python Dev",
+                            1000,
+                            "React Dev",
+                            1000,
+                            "Technology Enthusiast",
+                            1000,
+                            "Pokemon Fan",
+                            1000,
+                        ]}
+                        />
+                      </h1>
+                  </span>
+              </div>
+              
           </div>
       </div>
   </div>;
