dd33b6ae31b7c765a23e0bd8962a5ff937559996
agustinfranchetti <agustinfranchetti@gmail.com>
2022-02-08 17:08:55 -0300

    added necesarry folder structure. Cleaned up files

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Creates the main app component
 ReactDOM.render(
   <React.StrictMode>
     <App />
