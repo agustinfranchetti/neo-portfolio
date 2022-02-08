73c86f5b92e76799276015cbdff329b6fec2bdef
agustinfranchetti <agustinfranchetti@gmail.com>
2022-02-08 17:22:15 -0300

    added favicons inside Profile()

---
+++ portfolio/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Profile() {
+  return <div className='profile-container'>
+      <div className='profile-parent'>
+          <div className='profile-details'>
+              <div className='colz'>
+                  <a href='#'>
+                      <i className='fa fa-instagram'></i>
+                  </a>
+                  <a href='#'>
+                      <i className='fa fa-twitter-square'></i>
+                  </a>
+                  <a href='#'>
+                      <i className='fa fa-linkedin-square'></i>
+                  </a>
+                  <a href='#'>
+                      <i className='fa fa-github-square'></i>
+                  </a>
+              </div>
+          </div>
+      </div>
+  </div>;
+}
