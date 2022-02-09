86d7f141ac6ab75515bd2dd53f5ec1fd89f1360c
agustinfranchetti <agustinfranchetti@gmail.com>
2022-02-09 02:59:34 -0300

    added new component :)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import Profile from './PortfolioContainer/Home/Profile'
+import HeaderImage from './PortfolioContainer/HeaderImage/HeaderImage'
 
 function App() {
   return (
+    <div className='header-image'>
+     <HeaderImage/>
     <div className="App">
      <Profile/>
+    </div>
     </div>
   );
 }
---
+++ portfolio/src/PortfolioContainer/HeaderImage/HeaderImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HeaderImage.css'
+
+function HeaderImage() {
+    return (
+        <div>
+            <img src="geek.svg" className='geek-image'/>
+        </div>
+    )
+}
+export default HeaderImage
